01a3d5ff0e19f18fcb2070ff0bc9d362edf96fb1
kcj100 <kaljohnson2003@gmail.com>
2023-09-13 20:23:08 -0400

    update sout statement

--- src/main/java/com/kcj/aniproject/AniProject.java
+++ src/main/java/com/kcj/aniproject/AniProject.java
@@ -13,7 +13,7 @@ public class AniProject {
         Animal dog = new Animal(new StringBuilder("Dog"), "Rocky", new String[]{"woof!", "roof!", "grrr!"});
         Animal cat = new Animal(new StringBuilder("Cat"), "Misty", new String[]{"meow!", "purr...", "hissss!"});
         Animal horse = new Animal(new StringBuilder("Horse"), "Thunder", new String[]{"neigh!", "pfff!", "brrr!"});
-        
+
         dog.printAnimalSound();
         System.out.println("");
 
@@ -22,9 +22,9 @@ public class AniProject {
 
         horse.printAnimalSound();
         System.out.println("");
-        
+
         System.out.println("I like those sounds that the animals make. Can they make more?");
-        System.out.println("\nYes, indeed they can. Here is everything in total:\n");
+        System.out.println("\nYes, indeed they can. Here are some more:\n");
         dog.printDescriptions();
         dog.printAllAnimalSounds();
         System.out.println("");
@@ -33,7 +33,6 @@ public class AniProject {
         System.out.println("");
         horse.printDescriptions();
         horse.printAllAnimalSounds();
-        
-        
+
     }
 }
